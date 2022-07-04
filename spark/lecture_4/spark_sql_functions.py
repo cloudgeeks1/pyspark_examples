62f35eebef1ae67e2c7fefb0527408a9597911a7
pratik.joshi@marketcast.com <pratik.joshi@marketcast.com>
2022-07-04 09:20:19 +0530

    new sample examples

--- spark/lecture_4/spark_sql_functions.py
+++ spark/lecture_4/spark_sql_functions.py
@@ -3,13 +3,13 @@ from pyspark.sql import SparkSession
 
 # Create SparkSession
 spark = SparkSession.builder \
-      .master("local[1]") \
-      .appName("cloudgeeks") \
-      .getOrCreate()
+    .master("local[1]") \
+    .appName("cloudgeeks") \
+    .getOrCreate()
 
 
 df = spark.read.option("header","true").json(
-      "/Users/pratikjoshi/PycharmProjects/cloudgeeks_pyspark_examples/data/wikidata_type_dict.json")
+    "/Users/pratikjoshi/PycharmProjects/cloudgeeks_pyspark_examples/data/wikidata_type_dict.json")
 
 print(df.select("petalLength").show())
 
